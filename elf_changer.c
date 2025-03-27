1e9145ef42366476f4645460246107ab1b563847
GalPovsod8 <galpos10@gmail.com>
2025-03-27 15:22:11 +0100

    implemented glava function

--- elf_changer.c
+++ elf_changer.c
@@ -18,6 +18,16 @@ void elf_28878_glava(char* zacetekElf){
         close(fd);
         exit(EXIT_FAILURE);
     }
+
+    printf("ELF header\n");
+    printf("  Magic number:       %x %x %x %x\n", ehdr.e_ident[0], ehdr.e_ident[1], ehdr.e_ident[2], ehdr.e_ident[3]);
+    printf("  Class:             %s\n", (ehdr.e_ident[EI_CLASS] == ELFCLASS64) ? "64-bit" : "32-bit");
+    printf("  Data:              %s\n", (ehdr.e_ident[EI_DATA] == ELFDATA2LSB) ? "Little Endian" : "Big Endian");
+    printf("  Type:              %d\n", ehdr.e_type);
+    printf("  Machine:           %d\n", ehdr.e_machine);
+    printf("  Entry point:       0x%lx\n", ehdr.e_entry);
+
+    close(fd);
 }
 
 void elf_28878_simboli(char* zacetekElf){
