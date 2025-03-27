97ef6c96acddcceb8be8c0091ca7e80e3e74b29b
GalPovsod8 <galpos10@gmail.com>
2025-03-27 15:19:44 +0100

    started implementing glava function

--- elf_changer.c
+++ elf_changer.c
@@ -6,7 +6,11 @@
 #include "elf_changer.h"
 
 void elf_28878_glava(char* zacetekElf){
-
+    int fd = open(zacetekElf, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
 }
 
 void elf_28878_simboli(char* zacetekElf){
