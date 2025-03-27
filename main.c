68f63323872d4023e30cbce870c24724f6541a75
GalPovsod8 <galpos10@gmail.com>
2025-03-27 14:53:27 +0100

    added basic function declarations

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 #include <unistd.h>
 #include <string.h>
 
+void elf_28878_glava(char* zacetekElf){
+    //-h
+}
+
+void elf_28878_simboli(char* zacetekElf){
+    //-l
+}
+
+void elf_28878_menjaj(char* zacetekElf){
+    //-c
+}
+
 int main(int argc, char *argv[]){
 
 }
